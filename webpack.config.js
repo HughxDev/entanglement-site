89d51dbb346045a1835ec18635e6d9719c13f0cc
Hugh Guiney <hugh@hughguiney.com>
2020-09-12 21:44:01 -0400

    Dev MVP + Signup form

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const webpack = require( 'webpack' );
 const path = require( 'path' );
+const glob = require( 'glob' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 const CopyPlugin = require( 'copy-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const PurgeCSSPlugin = require( 'purgecss-webpack-plugin' );
+
+// const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   // "entry": path.join( __dirname, "src/index.html" ),
@@ -15,6 +20,56 @@ module.exports = {
     "compress": true,
     "port": 9000,
   },
+  "module": {
+    "rules": [
+      {
+        "test": /\.(sa|sc|c)ss$/,
+        "use": [
+          {
+            "loader": MiniCssExtractPlugin.loader,
+            "options": {
+              "hmr": process.env.NODE_ENV === 'development',
+            },
+          },
+          {
+            "loader": 'css-loader',
+            "options": {
+              "importLoaders": 2,
+            },
+          },
+          // 'postcss-loader',
+          {
+            "loader": 'postcss-loader', // Run post css actions
+            "options": {
+              "postcssOptions": {
+                "plugins": function () { // post css plugins, can be exported to postcss.config.js
+                  return [
+                    require( 'precss' ),
+                    require( 'autoprefixer' ),
+                    // [
+                    //   "@fullhuman/postcss-purgecss", // use @fullhuman/postcss-purgecss
+                    //   {
+                    //     "content": [
+                    //       path.join( __dirname, "./public/index.html" ),
+                    //     ],
+                    //   },
+                    // ],
+                  ];
+                },
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        "test": /\.(png|svg|jpe?g|gif|webp)$/,
+        "use": [
+          'file-loader',
+        ],
+      },
+    ],
+  },
   "plugins": [
     new HtmlWebpackPlugin( {
       "template": "public/index.html",
@@ -24,8 +79,13 @@ module.exports = {
       "patterns": [
         {
           "from": "public",
+          "to": "dist",
         },
       ],
+    } ),
+    new MiniCssExtractPlugin(),
+    new PurgeCSSPlugin( {
+      "paths": glob.sync( `public/index.html`, { "nodir": true } ),
     } ),
   ],
 };
